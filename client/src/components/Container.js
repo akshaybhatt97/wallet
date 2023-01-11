e9bc51439e419be0dc152d1d4444bf46be6d228e
akshaybhatt97 <81355505+akshaybhatt97@users.noreply.github.com>
2023-01-11 19:16:15 +0530

    Update Container.js

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-  background: #945CBE;
+  background: #50b8da;
   width: 375px;
   height: 100vh;
   margin: auto;
@@ -10,7 +10,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  ${ ({center}) => center && "justify-content: center"};
+  ${({ center }) => center && "justify-content: center"};
 `;
 
 export default Container;
